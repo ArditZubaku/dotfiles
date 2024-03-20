913872d87b3086a275f4e3ed0e69d69e9f749e20
Ardit Zubaku <63038966+ArditZubaku@users.noreply.github.com>
2024-03-20 07:35:13 +0100

    nvim config files

---
+++ nvim/lua/plugins/toggleterm.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "akinsho/toggleterm.nvim",
+    config = function()
+      require("toggleterm").setup({
+        -- Your configuration here
+        size = 20,
+        open_mapping = [[<C-`>]],
+        hide_numbers = true,
+        shade_filetypes = {},
+        shade_terminals = true,
+        shading_factor = 2,
+        start_in_insert = true,
+        insert_mappings = true,
+        persist_size = true,
+        direction = "horizontal",
+        close_on_exit = true,
+        shell = vim.o.shell,
+      })
+    end,
+  },
+}
